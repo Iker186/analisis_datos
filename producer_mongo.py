c91b7299fe4a9f9e831939ee57f5dc3e69a55b36
Iker186 <sancheziker549@gmail.com>
2025-04-06 03:42:11 -0600

    kafka 4

--- producer_mongo.py
+++ producer_mongo.py
@@ -3,12 +3,12 @@ from kafka import KafkaProducer
 import json
 import os
 
-KAFKA_BROKER = os.getenv('KAFKA_SERVER')
+KAFKA_SERVER = os.getenv('KAFKA_SERVER')
 TOPIC = os.getenv('KAFKA_TOPIC', 'results_topic')
 
 # Configuración del productor
 producer = KafkaProducer(
-    bootstrap_servers=KAFKA_BROKER,
+    bootstrap_servers=KAFKA_SERVER,
     security_protocol=os.getenv("KAFKA_SECURITY_PROTOCOL", "SASL_SSL"),  # Usamos SASL_SSL por Redpanda
     sasl_mechanism=os.getenv("KAFKA_SASL_MECHANISM", "SCRAM-SHA-256"),  # O SCRAM-SHA-512 según lo que hayas configurado
     sasl_plain_username=os.getenv("KAFKA_USERNAME"),
